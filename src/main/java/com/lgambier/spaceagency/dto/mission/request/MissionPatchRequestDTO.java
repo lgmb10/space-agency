525e1eb1c099cd528c092cff4e328286d254c276
Louis Gambier <louisgambier10@gmail.com>
2026-01-21 16:17:45 +0100

    Feat US 3.2
    Création nouveaux endpoints :
    /missions PATCH
    /missions/status PATCH
    
    Création exception pour transitions de statut de mission
    Ajouts tests unitaires pour Mission
    
    Rename d'exceptions
    Ajout formateur d'exception pour les MethodArgumentNotValidException
    
    # Conflicts:
    #	bruno/SpaceAgency Mission/Patch.bru
    #	bruno/SpaceAgency Mission/Update status.bru
    #	src/main/java/com/lgambier/spaceagency/controllers/MissionController.java
    #	src/main/java/com/lgambier/spaceagency/controllers/PassengerController.java
    #	src/main/java/com/lgambier/spaceagency/controllers/ShipController.java
    #	src/main/java/com/lgambier/spaceagency/dto/mission/request/MissionPatchRequestDTO.java
    #	src/main/java/com/lgambier/spaceagency/exceptions/config/GlobalExceptionHandler.java
    #	src/main/java/com/lgambier/spaceagency/exceptions/mission/MissionShipTimeSlotAlreadyInUseException.java
    #	src/main/java/com/lgambier/spaceagency/services/MissionService.java
    #	src/main/java/com/lgambier/spaceagency/services/ShipService.java
    #	src/test/java/com/lgambier/spaceagency/services/MissionServiceTest.java
    #	src/test/java/com/lgambier/spaceagency/services/ShipServiceTest.java

--- src/main/java/com/lgambier/spaceagency/dto/mission/request/MissionPatchRequestDTO.java
+++ src/main/java/com/lgambier/spaceagency/dto/mission/request/MissionPatchRequestDTO.java
@@ -1,7 +1,10 @@
 package com.lgambier.spaceagency.dto.mission.request;
 
 import com.fasterxml.jackson.annotation.JsonInclude;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
 import lombok.NoArgsConstructor;
+import lombok.Setter;
 import lombok.experimental.SuperBuilder;
 
 @NoArgsConstructor

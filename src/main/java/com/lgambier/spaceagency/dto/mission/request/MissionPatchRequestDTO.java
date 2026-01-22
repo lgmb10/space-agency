d03a0d2386d0c3e6d575feca580c42bcab17e20b
Louis Gambier <louisgambier10@gmail.com>
2026-01-22 11:53:21 +0100

    fix with DTO
    # Conflicts:
    #	src/main/java/com/lgambier/spaceagency/controllers/MissionController.java
    #	src/main/java/com/lgambier/spaceagency/services/MissionService.java
    #	src/test/java/com/lgambier/spaceagency/services/MissionServiceTest.java

--- src/main/java/com/lgambier/spaceagency/dto/mission/request/MissionPatchRequestDTO.java
+++ src/main/java/com/lgambier/spaceagency/dto/mission/request/MissionPatchRequestDTO.java
@@ -1,10 +1,7 @@
 package com.lgambier.spaceagency.dto.mission.request;
 
 import com.fasterxml.jackson.annotation.JsonInclude;
-import lombok.AllArgsConstructor;
-import lombok.Getter;
 import lombok.NoArgsConstructor;
-import lombok.Setter;
 import lombok.experimental.SuperBuilder;
 
 @NoArgsConstructor

55f9c95c061e3db5e70b9c2bac4052a048ddc8f2
Louis Gambier <louisgambier10@gmail.com>
2026-02-03 17:05:26 +0100

    feat: ajout de query params pour la pagination et le filtrage sur l'endpoint GET /passengers

--- src/main/java/com/lgambier/spaceagency/config/AppConfig.java
+++ src/main/java/com/lgambier/spaceagency/config/AppConfig.java
@@ -4,7 +4,11 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.support.PropertySourcesPlaceholderConfigurer;
 import org.springframework.core.io.FileSystemResource;
+import org.springframework.data.web.config.EnableSpringDataWebSupport;
 
+import static org.springframework.data.web.config.EnableSpringDataWebSupport.PageSerializationMode.VIA_DTO;
+
+@EnableSpringDataWebSupport(pageSerializationMode = VIA_DTO)
 @Configuration
 public class AppConfig {
 
